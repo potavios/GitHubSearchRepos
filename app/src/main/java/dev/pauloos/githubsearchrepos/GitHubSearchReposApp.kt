df186a5fe89acfbaad3facc50100d2f530267709
Paulo Santos <pauloos@live.com>
2022-06-04 09:21:15 -0300

    Tetste

--- app/src/main/java/dev/pauloos/githubsearchrepos/GitHubSearchReposApp.kt
+++ app/src/main/java/dev/pauloos/githubsearchrepos/GitHubSearchReposApp.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GitHubSearchReposApp() : Application()
+class GitHubSearchReposApp()   : Application()
